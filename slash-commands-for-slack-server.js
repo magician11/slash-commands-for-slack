6a6c8116b59d3d6631ad9ba3ae3b22e8eb14941a
Andrew Golightly <andrew@golightlyplus.com>
2016-01-21 10:42:01 +0700

    getting project ID from formstack for channel

--- slash-commands-for-slack-server.js
+++ slash-commands-for-slack-server.js
@@ -10,6 +10,7 @@ const PORT = 8080;
 const FRESHBOOKS_API_URL = process.env.SUNBOWL_FRESHBOOKS_URL;
 const FRESHBOOKS_AUTH_KEY = process.env.SUNBOWL_FRESHBOOKS_API_TOKEN;
 const SLACK_TOKEN = process.env.SUNBOWL_SLACK_TOKEN;
+const FORMSTACK_TOKEN = process.env.SUNBOWL_FORMSTACK_TOKEN;
 
 // objects to use in the app
 let freshbooks = new FreshBooks(FRESHBOOKS_API_URL, FRESHBOOKS_AUTH_KEY);
@@ -28,49 +29,47 @@ app.get('/bucket', function(req, res) {
   }
 
   // get the Slack channel name / Freshbook project ID pairs
-  client.get("https://www.formstack.com/api/v2/form/2198788/submission.json?data=true&oauth_token=b9400c279dd0dfcd22dc47c3282c7173", function(data, response){
+  client.get(`https://www.formstack.com/api/v2/form/2198788/submission.json?data=true&oauth_token=${FORMSTACK_TOKEN}`, function(data, response){
 
-    let formData = JSON.parse(data.toString('utf8'));
-
-    formData.submissions.forEach(function(submission) {
+    data.submissions.forEach(function(submission) {
       channelNameAndFreshBookIDPair[submission.data[38710905].value] = submission.data[38710988].value;
     });
 
-    console.log(channelNameAndFreshBookIDPair);
-  });
+    // then, get the project details (given project_id retrieve name and budget)
+    let projects = new freshbooks.Project();
+    let projectID = channelNameAndFreshBookIDPair[req.query.channel_name];
+    projects.get(projectID, function(err, project) {
 
-  // get the project details (project_id, name, budget)
-  let projects = new freshbooks.Project();
-  let projectID = parseInt(req.query.text);
-  projects.get(projectID, function(err, project) {
-
-    // catch any project ID errors (e.g. NAN or project ID not found)
-    if(err) {
-      respondWithError(err, res);
-      return;
-    }
-
-    // get the times entered for this project
-    let times = new freshbooks.Time_Entry();
-    times.list({project_id: projectID, per_page: 99999}, function(err, times) {
-
-      // sum the hours that are billable
-      let billableHours = 0;
-      for(let time of times) {
-        billableHours += parseFloat(time.hours);
+      // catch any project ID errors (e.g. NAN or project ID not found)
+      if(err) {
+        respondWithError(err, res);
+        return;
       }
 
-      // return the JSON for this request
-      res.json({
-        text: `You have used ${billableHours.toFixed(1)} hours of your ${parseInt(project.budget)} hour budget.`,
-        'attachments': [
-          {
-            text: `This is for ${project.name} with ID ${project.project_id}.`
-          }
-        ]
+      // get the times entered for this project
+      let times = new freshbooks.Time_Entry();
+      times.list({project_id: projectID, per_page: 99999}, function(err, times) {
+
+        // sum the hours that are billable
+        let billableHours = 0;
+        for(let time of times) {
+          billableHours += parseFloat(time.hours);
+        }
+
+        // return the JSON for this request
+        res.json({
+          text: `You have used ${billableHours.toFixed(1)} hours of your ${parseInt(project.budget)} hour budget.`,
+          'attachments': [
+            {
+              text: `This is for ${project.name} with ID ${project.project_id}.`
+            }
+          ]
+        });
       });
     });
+
   });
+
 });
 
 // utility functions
