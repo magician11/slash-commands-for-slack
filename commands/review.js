d641235d333762a3467147e94307d52109d20872
Andrew Golightly <andrew@golightlyplus.com>
2016-03-16 13:21:35 +1300

    added task id comment

--- commands/review.js
+++ commands/review.js
@@ -67,7 +67,7 @@ module.exports = (app) => {
         trello.get(`/1/checklists/${checklistId}/checkitems`, (err, data) => {
           let tasks = [];
           data.forEach((task) => {
-            tasks.push({ title: task.name, text: task.id });
+            tasks.push({ title: task.name, text: `Task ID: ${task.id}` });
           });
 
           res.json({
