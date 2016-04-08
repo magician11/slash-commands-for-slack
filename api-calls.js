0da3b229bde1f79072039b925dc35c85a197ecbc
Andrew Golightly <andrew@golightlyplus.com>
2016-04-08 12:52:26 +1000

    refactored bucket code to api-calls file

--- api-calls.js
+++ api-calls.js
@@ -4,9 +4,11 @@
 The various API calls to freshbooks, trello and formstack
 */
 
+/* GLOBALS FOR API CALLS */
+const FORMSTACK_TOKEN = process.env.SUNBOWL_FORMSTACK_TOKEN;
+
 /* FORMSTACK */
 const request = require('request');
-const FORMSTACK_TOKEN = process.env.SUNBOWL_FORMSTACK_TOKEN;
 
 // grab the trello card ID associated with the passed in channel name
 const getTrelloCardId = (channelName) => {
@@ -35,98 +37,186 @@ const getTrelloCardId = (channelName) => {
     });
   };
 
-  /* FRESHBOOKS */
-
-  /* TRELLO */
-
-  const Trello = require('node-trello');
-  const TRELLO_APP_KEY = process.env.SUNBOWL_TRELLO_KEY;
-  const TRELLO_USER_TOKEN = process.env.SUNBOWL_TRELLO_TOKEN;
-  const trello = new Trello(TRELLO_APP_KEY, TRELLO_USER_TOKEN);
-
-  // find out which checklist is called "Incoming" for the specified card ID
-  const getTaskListId = (trelloCardId) => {
+  // get the freshbooks ID associated with this channel name from formstack
+  const getFreshbooksProjectId = (channelName) => {
     return new Promise((resolve, reject) => {
-      trello.get(`/1/cards/${trelloCardId}/checklists`, (err, data) => {
-        const CHECKLIST_NAME = 'Incoming';
-        let checklistId;
-        data.forEach((checklist) => {
-          if (checklist.name === CHECKLIST_NAME) {
-            checklistId = checklist.id;
+      request.get({
+        url: `https://www.formstack.com/api/v2/form/2198788/submission.json?data=true&per_page=100&oauth_token=${FORMSTACK_TOKEN}`,
+        json: true },
+        (error, response, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            const channelNameAndFreshBookIDPair = {};
+            data.submissions.forEach((submission) => {
+              channelNameAndFreshBookIDPair[submission.data[38710905].value] = submission.data[38710988].value;
+            });
+            // freshbooksData.projectId = channelNameAndFreshBookIDPair[channelName];
+            if (channelNameAndFreshBookIDPair[channelName]) {
+              resolve(channelNameAndFreshBookIDPair[channelName]);
+            } else {
+              reject(`${channelName} is not registered with formstack.`);
+            }
           }
         });
+      });
+    };
 
-        if (checklistId) {
-          resolve(checklistId);
-        } else {
-          // if it doesn't exist, create the Incoming checklist and resolve with that ID
-          trello.post(`/1/cards/${trelloCardId}/checklists`, { name: CHECKLIST_NAME }, function(err, data) {
-            if (err) {
-              reject(err);
+    /* FRESHBOOKS */
+    const FreshBooks = require('freshbooks');
+    const FRESHBOOKS_API_URL = process.env.SUNBOWL_FRESHBOOKS_URL;
+    const FRESHBOOKS_AUTH_KEY = process.env.SUNBOWL_FRESHBOOKS_API_TOKEN;
+    const freshbooks = new FreshBooks(FRESHBOOKS_API_URL, FRESHBOOKS_AUTH_KEY);
+
+    const getProjectBudget = (projectId) => {
+      return new Promise((resolve, reject) => {
+        const projects = new freshbooks.Project();
+        projects.get(projectId, (err, project) => {
+          if (err) {
+            reject('Could not find a project setup for this channel in Freshbooks.');
+          } else {
+            resolve(parseInt(project.budget, 10));
+          }
+        });
+      });
+    };
+
+    const getBillableHours = (projectId) => {
+      function sumTimes(times) {
+        let billableHours = 0;
+        for (const time of times) {
+          billableHours += parseFloat(time.hours);
+        }
+
+        return billableHours;
+      }
+
+      return new Promise((resolve, reject) => {
+        const timeEntries = new freshbooks.Time_Entry();
+        let billableHours = 0;
+        timeEntries.list({ project_id: projectId, per_page: 100 }, (err, times, options) => {
+          if (err) {
+            reject(err);
+          } else {
+            // grab the first page of times
+            billableHours = sumTimes(times);
+
+            // if there are more pages to process, get those...
+            if (options.pages > 1) {
+              const pagesToProcess = [];
+              for (let i = 2; i <= options.pages; i++) {
+                pagesToProcess.push(new Promise((done) => {
+                  timeEntries.list({ project_id: projectId, per_page: 100, page: i }, (error, moreTimes) => {
+                    const extraHours = sumTimes(moreTimes);
+
+                    done(extraHours);
+                  });
+                }));
+              }
+
+              Promise.all(pagesToProcess).then((extraTimes) => {
+                billableHours = billableHours + extraTimes.reduce((a, b) => { return a + b; });
+                resolve(billableHours);
+              });
+            } else {
+              resolve(billableHours);
             }
-            resolve(data.id);
+          }
+        });
+      });
+    };
+
+    /* TRELLO */
+
+    const Trello = require('node-trello');
+    const TRELLO_APP_KEY = process.env.SUNBOWL_TRELLO_KEY;
+    const TRELLO_USER_TOKEN = process.env.SUNBOWL_TRELLO_TOKEN;
+    const trello = new Trello(TRELLO_APP_KEY, TRELLO_USER_TOKEN);
+
+    // find out which checklist is called "Incoming" for the specified card ID
+    const getTaskListId = (trelloCardId) => {
+      return new Promise((resolve, reject) => {
+        trello.get(`/1/cards/${trelloCardId}/checklists`, (err, data) => {
+          const CHECKLIST_NAME = 'Incoming';
+          let checklistId;
+          data.forEach((checklist) => {
+            if (checklist.name === CHECKLIST_NAME) {
+              checklistId = checklist.id;
+            }
           });
-        }
+
+          if (checklistId) {
+            resolve(checklistId);
+          } else {
+            // if it doesn't exist, create the Incoming checklist and resolve with that ID
+            trello.post(`/1/cards/${trelloCardId}/checklists`, { name: CHECKLIST_NAME }, (error, incomingChecklist) => {
+              if (error) {
+                reject(error);
+              }
+              resolve(incomingChecklist.id);
+            });
+          }
+        });
       });
-    });
-  };
+    };
 
-  // add a task to the tasklist
-  const addTask = (taskListId, task) => {
-    return new Promise((resolve, reject) => {
-      trello.post(`/1/checklists/${taskListId}/checkitems`, { name: task }, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
+    // add a task to the tasklist
+    const addTask = (taskListId, task) => {
+      return new Promise((resolve, reject) => {
+        trello.post(`/1/checklists/${taskListId}/checkitems`, { name: task }, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
       });
-    });
-  };
+    };
 
-  const utils = require('./utils');
+    const utils = require('./utils');
 
-  const renameTasklist = (taskListId, assignee) => {
-    return new Promise((resolve, reject) => {
-      const date = utils.dateXdaysFromNow(2);
-      trello.put(`/1/checklists/${taskListId}/name`, { value: `${assignee} - ${date}` }, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data.id);
-        }
+    const renameTasklist = (taskListId, assignee) => {
+      return new Promise((resolve, reject) => {
+        const date = utils.dateXdaysFromNow(2);
+        trello.put(`/1/checklists/${taskListId}/name`, { value: `${assignee} - ${date}` }, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data.id);
+          }
+        });
       });
-    });
-  };
+    };
 
-  // move the trello card to the "Pending to be Assigned" list
-  const moveTrelloCard = (trelloCardId) => {
-    return new Promise((resolve, reject) => {
-      const PENDING_TO_BE_ASSIGNED_LIST_ID = '537bc2cec1db170a09078963';
-      trello.put(`/1/cards/${trelloCardId}/idList`, { value: PENDING_TO_BE_ASSIGNED_LIST_ID }, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data.id);
-        }
+    // move the trello card to the "Pending to be Assigned" list
+    const moveTrelloCard = (trelloCardId) => {
+      return new Promise((resolve, reject) => {
+        const PENDING_TO_BE_ASSIGNED_LIST_ID = '537bc2cec1db170a09078963';
+        trello.put(`/1/cards/${trelloCardId}/idList`, { value: PENDING_TO_BE_ASSIGNED_LIST_ID }, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data.id);
+          }
+        });
       });
-    });
-  };
+    };
 
-  // fetch the tasks from a specific check list and return it as a bulleted string
-  const getTaskListItems = (checklistId) => {
-    return new Promise((resolve, reject) => {
-      trello.get(`/1/checklists/${checklistId}/checkitems`, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
+    // fetch the tasks from a specific check list and return it as a bulleted string
+    const getTaskListItems = (checklistId) => {
+      return new Promise((resolve, reject) => {
+        trello.get(`/1/checklists/${checklistId}/checkitems`, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
       });
-    });
-  };
+    };
 
-  /* export the api functions */
-  module.exports = {
-    getTrelloCardId, getTaskListId, getTaskListItems, addTask, moveTrelloCard, renameTasklist
-  };
+    /* export the api functions */
+    module.exports = {
+      getTrelloCardId, getTaskListId, getTaskListItems, getProjectBudget,
+      addTask, moveTrelloCard, renameTasklist, getFreshbooksProjectId, getBillableHours
+    };
--- commands/bucket.js
+++ commands/bucket.js
@@ -1,48 +1,41 @@
 'use strict';
 
 module.exports = (app) => {
-
-  let FreshBooks = require("freshbooks");
-  let request = require('request');
-  let utils = require('../utils');
-
-  const FRESHBOOKS_API_URL = process.env.SUNBOWL_FRESHBOOKS_URL;
-  const FRESHBOOKS_AUTH_KEY = process.env.SUNBOWL_FRESHBOOKS_API_TOKEN;
+  const apiCalls = require('../api-calls');
+  const utils = require('../utils');
   const BUCKET_SECURITY_TOKEN = process.env.SUNBOWL_BUCKET_SECURITY_TOKEN;
-  const FORMSTACK_TOKEN = process.env.SUNBOWL_FORMSTACK_TOKEN;
 
   // get information about a bucket with Sunbowl
-  app.get('/bucket', function(req, res) {
-
+  app.get('/bucket', (req, res) => {
     // check to see whether this script is being accessed from our slack integration
-    if(req.query.token !== BUCKET_SECURITY_TOKEN) {
+    if (req.query.token !== BUCKET_SECURITY_TOKEN) {
       utils.respondWithError('Access denied.', res);
       return;
     }
 
     // switch on bucket option
-    if(req.query.text) {
-      switch(req.query.text) {
+    if (req.query.text) {
+      switch (req.query.text) {
         case 'refill':
 
         res.json({
-          text: `To refill your bucket, click on your bucket choice below...`,
-          'attachments': [
+          text: 'To refill your bucket, click on your bucket choice below...',
+          attachments: [
             {
-              "title": "14 hour bucket",
-              "title_link": "http://www.sunbowl.ca/14hb"
+              title: '14 hour bucket',
+              title_link: 'http://www.sunbowl.ca/14hb'
             },
             {
-              "title": "21 hour bucket",
-              "title_link": "http://www.sunbowl.ca/21hb"
+              title: '21 hour bucket',
+              title_link: 'http://www.sunbowl.ca/21hb'
             },
             {
-              "title": "28 hour bucket",
-              "title_link": "http://www.sunbowl.ca/28hb"
+              title: '28 hour bucket',
+              title_link: 'http://www.sunbowl.ca/28hb'
             },
             {
-              "title": "40 hour bucket",
-              "title_link": "http://www.sunbowl.ca/40hb"
+              title: '40 hour bucket',
+              title_link: 'http://www.sunbowl.ca/40hb'
             }
           ]
         });
@@ -52,122 +45,31 @@ module.exports = (app) => {
 
       }
     } else {
+      const freshbooksData = {};
 
-      let freshbooks = new FreshBooks(FRESHBOOKS_API_URL, FRESHBOOKS_AUTH_KEY);
-      let freshbooksData = {};
+      apiCalls.getFreshbooksProjectId(req.query.channel_name)
+      .then((freshbooksProjectId) => {freshbooksData.projectId = freshbooksProjectId; return apiCalls.getProjectBudget(freshbooksProjectId); })
+      .then((projectBudget) => {freshbooksData.projectBudget = projectBudget; return apiCalls.getBillableHours(freshbooksData.projectId);})
+      .then((billableHours) => {
+        const percentBucketUsed = (billableHours / freshbooksData.projectBudget) * 100;
+        const timeLeft = freshbooksData.projectBudget - billableHours;
+        const progressColour = (percentBucketUsed > 75) ? 'danger' : 'good';
 
-      const getFreshbooksProjectId = new Promise((resolve, reject) => {
-        request.get({
-          url: `https://www.formstack.com/api/v2/form/2198788/submission.json?data=true&per_page=100&oauth_token=${FORMSTACK_TOKEN}`,
-          json: true },
-          function(error, response, data) {
-            if(error) {
-              reject(error);
-            } else {
-
-              let channelNameAndFreshBookIDPair = {};
-              data.submissions.forEach(function(submission) {
-                channelNameAndFreshBookIDPair[submission.data[38710905].value] = submission.data[38710988].value;
-              });
-              freshbooksData.projectId = channelNameAndFreshBookIDPair[req.query.channel_name];
-              if(freshbooksData.projectId) {
-                resolve(channelNameAndFreshBookIDPair[req.query.channel_name]);
-              } else {
-                reject(`${req.query.channel_name} is not registered with formstack.`);
-              }
+        // return the JSON for this request
+        res.json({
+          text: `You have used \`${percentBucketUsed.toFixed()}%\` of your \`${freshbooksData.projectBudget} hour\` bucket.`,
+          attachments: [
+            {
+              color: progressColour,
+              text: `\`${timeLeft.toFixed(1)} hours\` left before you will need to top it up.`,
+              mrkdwn_in: ['text']
             }
-          });
+          ]
         });
-
-        let getProjectBudget = function(projectId) {
-          return new Promise((resolve, reject) => {
-            let projects = new freshbooks.Project();
-            projects.get(projectId, function(err, project) {
-              if(err) {
-                reject('Could not find a project setup for this channel in Freshbooks.');
-              } else {
-                freshbooksData.projectBudget = parseInt(project.budget);
-                resolve(projectId);
-              }
-            });
-          });
-        };
-
-        let getBillableHours = function(projectId) {
-
-          function sumTimes(times) {
-
-            let billableHours = 0;
-            for(let time of times) {
-              billableHours += parseFloat(time.hours);
-            }
-
-            return billableHours;
-          }
-
-          return new Promise((resolve, reject) => {
-            let timeEntries = new freshbooks.Time_Entry();
-            let billableHours = 0;
-            timeEntries.list({project_id: projectId, per_page: 100}, function(err, times, options) {
-
-              if(err) {
-                reject(err);
-              } else {
-
-                // grab the first page of times
-                billableHours = sumTimes(times);
-
-                // if there are more pages to process, get those...
-                if(options.pages > 1) {
-                  let pagesToProcess = [];
-                  for(let i = 2; i <= options.pages; i++) {
-                    pagesToProcess.push(new Promise((done, reject) => {
-                      timeEntries.list({project_id: projectId, per_page: 100, page: i }, function(err, times, options) {
-                        let extraHours = sumTimes(times);
-
-                        done(extraHours);
-                      });
-                    }));
-                  }
-
-                  Promise.all(pagesToProcess).then((extraTimes) => {
-
-                    freshbooksData.billableHours = billableHours + extraTimes.reduce(function(a,b) { return a + b; });
-                    resolve(freshbooksData.billableHours);
-                  });
-                } else {
-                  freshbooksData.billableHours = billableHours;
-                  resolve(freshbooksData.billableHours);
-                }
-              }
-            });
-          });
-        };
-
-        getFreshbooksProjectId
-        .then(getProjectBudget)
-        .then(getBillableHours)
-        .then(() => {
-
-          let percentBucketUsed = (freshbooksData.billableHours / freshbooksData.projectBudget) * 100;
-          let timeLeft = freshbooksData.projectBudget - freshbooksData.billableHours;
-          let progressColour = (percentBucketUsed > 75) ? 'danger' : 'good';
-
-          // return the JSON for this request
-          res.json({
-            text: `You have used \`${percentBucketUsed.toFixed()}%\` of your \`${freshbooksData.projectBudget} hour\` bucket.`,
-            'attachments': [
-              {
-                color: progressColour,
-                text: `\`${timeLeft.toFixed(1)} hours\` left before you will need to top it up.`,
-                mrkdwn_in: ["text"]
-              }
-            ]
-          });
-        })
-        .catch(function(err){
-          utils.respondWithError(`Error: ${err}`, res);
-        });
-      }
-    });
-  }
+      })
+      .catch((err) => {
+        utils.respondWithError(`Error: ${err}`, res);
+      });
+    }
+  });
+};
