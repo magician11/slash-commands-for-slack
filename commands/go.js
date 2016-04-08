473d949bbd62e7db21c56de2831106e688832da7
Andrew Golightly <andrew@golightlyplus.com>
2016-04-08 16:14:00 +1000

    adding bucket balance to go command output

--- commands/go.js
+++ commands/go.js
@@ -7,7 +7,6 @@ module.exports = (app) => {
 
   // assign the card for this channel to a dev and then move it to the 'pending to be assigned' list
   app.get('/go', (req, res) => {
-
     const users = req.query.text;
 
     // check to see whether this script is being accessed from our slack integration
@@ -25,23 +24,30 @@ module.exports = (app) => {
     const ccNotifications = users.split(' ').slice(1).join(' ');
 
     const channelName = req.query.channel_name;
+    const freshbooksData = {};
+    let tasks = [];
 
     apiCalls.getTrelloCardId(channelName)
     .then(apiCalls.moveTrelloCard)
     .then(apiCalls.getTaskListId)
     .then((taskListId) => { return apiCalls.renameTasklist(taskListId, assignee); })
     .then((taskListId) => { return apiCalls.getTaskListItems(taskListId); })
-    .then((taskList) => {
+    .then((taskList) => { tasks = taskList; return apiCalls.getFreshbooksProjectId(channelName); })
+    .then((freshbooksProjectId) => {freshbooksData.projectId = freshbooksProjectId; return apiCalls.getProjectBudget(freshbooksProjectId); })
+    .then((projectBudget) => {freshbooksData.projectBudget = projectBudget; return apiCalls.getBillableHours(freshbooksData.projectId);})
+    .then((billableHours) => {
+      const timeLeft = freshbooksData.projectBudget - billableHours;
       const dueDate = utils.dateXdaysFromNow(3);
 
       res.json({
         response_type: 'in_channel',
-        text: `*Your latest sprint has been assigned to ${assignee}*
+text: `*Your latest sprint has been assigned to ${assignee}*
 ${(ccNotifications.length > 0) ? `*cc: ${ccNotifications}*` : ''}
 If we have missed anything please let's us know by sending us a message in the #${channelName} channel.
 Expected date of completion is ${dueDate}.
 
-*Sprint details*${utils.createBulletListFromArray(taskList)}`
+*Sprint details*${utils.createBulletListFromArray(tasks)}
+Bucket balance: ${timeLeft.toFixed(1)} hours`
       });
     })
     .catch((error) => {
