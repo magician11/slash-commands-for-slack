e89d513bd6f86e40ea1051da7aa3aea8e8cc2d57
Andrew Golightly <andrew@golightlyplus.com>
2016-04-18 15:28:40 +1000

    fixed bucket public bug

--- commands/bucket.js
+++ commands/bucket.js
@@ -14,36 +14,28 @@ module.exports = (app) => {
     }
 
     // switch on bucket option
-    if (req.query.text) {
-      switch (req.query.text) {
-        case 'refill':
-
-        res.json({
-          text: 'To refill your bucket, click on your bucket choice below...',
-          attachments: [
-            {
-              title: '14 hour bucket',
-              title_link: 'http://www.sunbowl.ca/14hb'
-            },
-            {
-              title: '21 hour bucket',
-              title_link: 'http://www.sunbowl.ca/21hb'
-            },
-            {
-              title: '28 hour bucket',
-              title_link: 'http://www.sunbowl.ca/28hb'
-            },
-            {
-              title: '40 hour bucket',
-              title_link: 'http://www.sunbowl.ca/40hb'
-            }
-          ]
-        });
-        break;
-
-        default: utils.respondWithError(`*${req.query.text}* is not a recognised option for the bucket command.`, res);
-
-      }
+    if (req.query.text === 'refill') {
+      res.json({
+        text: 'To refill your bucket, click on your bucket choice below...',
+        attachments: [
+          {
+            title: '14 hour bucket',
+            title_link: 'http://www.sunbowl.ca/14hb'
+          },
+          {
+            title: '21 hour bucket',
+            title_link: 'http://www.sunbowl.ca/21hb'
+          },
+          {
+            title: '28 hour bucket',
+            title_link: 'http://www.sunbowl.ca/28hb'
+          },
+          {
+            title: '40 hour bucket',
+            title_link: 'http://www.sunbowl.ca/40hb'
+          }
+        ]
+      });
     } else {
       const freshbooksData = {};
 
