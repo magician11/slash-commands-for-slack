9f0efabcdaf694fedd06f5d0162934c6e0bde66f
Andrew Golightly <andrew@golightlyplus.com>
2016-04-27 18:30:24 +0700

    finished complete command

--- commands/complete.js
+++ commands/complete.js
@@ -7,9 +7,6 @@ module.exports = (app) => {
 
   // notify someone that a sprint for a channel is complete
   app.get('/complete', (req, res) => {
-    // res.json({
-    //   text: 'We\'re processing your request. One moment please...'
-    // });
     const completeArguments = req.query.text.split(' ');
 
     // check to see whether this script is being accessed from our slack integration
@@ -27,16 +24,19 @@ module.exports = (app) => {
     const bucketTimeQuoted = bucketTimes[1];
     const videoUrl = completeArguments[2];
 
+    const freshbooksData = {};
+
     apiCalls.getFreshbooksProjectId(req.query.channel_name)
     .then((freshbooksProjectId) => {freshbooksData.projectId = freshbooksProjectId; return apiCalls.getProjectBudget(freshbooksProjectId); })
     .then((projectBudget) => {freshbooksData.projectBudget = projectBudget; return apiCalls.getBillableHours(freshbooksData.projectId);})
     .then((billableHours) => {
-      const percentBucketUsed = (billableHours / freshbooksData.projectBudget) * 100;
       const timeLeft = freshbooksData.projectBudget - billableHours;
 
       res.json({
+        response_type: 'in_channel',
         text: `${recipient} *Your Sprint is Complete!*
-${videoUrl ? `Video overview of sprint: <${videoUrl}|click here>` : ''}
+We have completed the most recent changes on your site and we would like you to take a minute to review them.
+${videoUrl ? `For your convenience, and to improve communication, we have made a quick video to show you what the site looks like on our side.: <${videoUrl}|watch video>` : ''}
 Bucket Time Quoted: \`${bucketTimeQuoted}\`
 Bucket Time Used: \`${bucketTimeUsed}\`
 Bucket balance:  You have \`${timeLeft.toFixed(1)} hours\` left.`
