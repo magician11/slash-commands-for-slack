fa64e3d6ded85c65db5fe9ae0d416d06e0582376
Andrew Golightly <andrew@golightlyplus.com>
2016-06-06 14:39:32 +0700

    added user to the review command

--- commands/review.js
+++ commands/review.js
@@ -7,9 +7,14 @@ module.exports = (app) => {
 
   // get all the tasks from this channel's trello card
   app.get('/review', (req, res) => {
+    const reviewArguments = req.query.text.split(' ');
+
     // check to see whether this script is being accessed from our slack integration
     if (req.query.token !== REVIEW_SECURITY_TOKEN) {
       utils.respondWithError('Access denied.', res);
+      return;
+    } else if (reviewArguments.length !== 1) {
+      utils.respondWithError('Usage: /review [person to notify e.g. @bob]', res);
       return;
     }
 
@@ -18,35 +23,26 @@ module.exports = (app) => {
     .then(apiCalls.getTaskListItems)
     .then((taskList) => {
       if (taskList.length === 0) {
-        res.json({
-          response_type: (req.query.text === 'public') ? 'in_channel' : 'ephemeral',
-          text: 'You have no tasks listed.'
-        });
+        utils.respondWithError('No tasks were found.', res);
       } else {
         let taskMessage = `Your current tasks are...${utils.createBulletListFromArray(taskList)}`;
-        if (req.query.text === 'public') {
-          taskMessage += '\n*Please review the above sprint and let me know if it\'s ready to assign out.*';
-          const freshbooksData = {};
+        taskMessage += `\n*Hey ${reviewArguments[0]}, please review the above sprint and let me know if it's ready to assign out.*`;
+        const freshbooksData = {};
 
-          apiCalls.getFreshbooksProjectId(req.query.channel_name)
-          .then((freshbooksProjectId) => {freshbooksData.projectId = freshbooksProjectId; return apiCalls.getProjectBudget(freshbooksProjectId); })
-          .then((projectBudget) => {freshbooksData.projectBudget = projectBudget; return apiCalls.getBillableHours(freshbooksData.projectId);})
-          .then((billableHours) => {
-            const percentBucketUsed = (billableHours / freshbooksData.projectBudget) * 100;
-            const timeLeft = freshbooksData.projectBudget - billableHours;
+        apiCalls.getFreshbooksProjectId(req.query.channel_name)
+        .then((freshbooksProjectId) => {freshbooksData.projectId = freshbooksProjectId; return apiCalls.getProjectBudget(freshbooksProjectId); })
+        .then((projectBudget) => {freshbooksData.projectBudget = projectBudget; return apiCalls.getBillableHours(freshbooksData.projectId);})
+        .then((billableHours) => {
+          const percentBucketUsed = (billableHours / freshbooksData.projectBudget) * 100;
+          const timeLeft = freshbooksData.projectBudget - billableHours;
 
-            taskMessage += `\n\`You have used ${percentBucketUsed.toFixed(0)}% of your bucket (${timeLeft.toFixed(1)} hours left)\``;
+          taskMessage += `\n\`You have used ${percentBucketUsed.toFixed(0)}% of your bucket (${timeLeft.toFixed(1)} hours left)\``;
 
-            res.json({
-              response_type: 'in_channel',
-              text: `${taskMessage}`
-            });
-          });
-        } else {
           res.json({
+            response_type: 'in_channel',
             text: `${taskMessage}`
           });
-        }
+        });
       }
     })
     .catch((error) => {
