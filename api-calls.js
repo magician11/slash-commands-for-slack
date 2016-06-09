c222be895834bfff4a6d70b9fe781e7d9f94956b
Andrew Golightly <andrew@golightlyplus.com>
2016-06-09 17:54:47 +0700

    search on username

--- api-calls.js
+++ api-calls.js
@@ -91,7 +91,7 @@ const getTrelloCardId = (channelName) => {
     const getUsersFreshbooksDetails = (userName) => {
       return new Promise((resolve, reject) => {
         request.get({
-          url: `https://www.formstack.com/api/v2/form/2350444/submission.json?data=true&search_field_0=41935293&search_value_0=${channelName}&oauth_token=${FORMSTACK_TOKEN}`,
+          url: `https://www.formstack.com/api/v2/form/2350444/submission.json?data=true&search_field_0=41935293&search_value_0=${userName}&oauth_token=${FORMSTACK_TOKEN}`,
           json: true },
           (error, response, data) => {
             if (error) {
