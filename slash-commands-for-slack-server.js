6a3c3a4fe8cd72c08a189ad1763eca6ffc13564e
Andrew Golightly <support@andrewgolightly.com>
2017-01-18 09:18:40 +0700

    updated port for dev branch

--- slash-commands-for-slack-server.js
+++ slash-commands-for-slack-server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-const PORT = 5555;
+
+// changed port for dev setup
+const PORT = 7777;
 
 const app = express();
 
@@ -17,5 +19,6 @@ require('./commands/que')(app);
 
 // start the server
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`Listening on port ${PORT}`);
 });
