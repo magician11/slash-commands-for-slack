6aca3e375a09234cb1566c0ff525c9918b3c781e
Andrew Golightly <support@andrewgolightly.com>
2017-05-09 21:24:03 -0600

    update jobreport

--- commands/jobreport.js
+++ commands/jobreport.js
@@ -1,14 +1,20 @@
-module.exports = (app) => {
+module.exports = app => {
   const utils = require('../modules/utils');
   const trelloSunbowl = require('../modules/trello');
+  const freshbooksSunbowl = require('../modules/freshbooks');
   const formstackSunbowl = require('../modules/formstack');
   const slackSunbowl = require('../modules/slack');
   const JOBREPORT_SECURITY_TOKEN = process.env.SUNBOWL_JOBREPORT_SECURITY_TOKEN;
 
+  /* eslint-disable max-len */
+
   // notify someone that a sprint for a channel is complete
   app.get('/jobreport', (req, res) => {
     const userName = req.query.user_name;
+    const channelName = req.query.channel_name;
     const jobreportArguments = req.query.text.split(' ');
+    const timeTaken = jobreportArguments[0];
+    const videoUrl = jobreportArguments[1];
 
     // check to see whether this script is being accessed from our slack integration
     if (req.query.token !== JOBREPORT_SECURITY_TOKEN) {
@@ -19,23 +25,39 @@ module.exports = (app) => {
       return;
     }
 
-    res.json({ text: `Thanks <@${userName}>. Your job report has been submitted.` });
+    res.json({
+      text: `Thanks <@${userName}>. Your job report has been submitted.`
+    });
 
     const finishedBlockListId = '522e91fe2c1df8cb25008ab2';
 
-    formstackSunbowl.getTrelloCardId(req.query.channel_name)
-    .then((trelloCardId) => trelloSunbowl.moveTrelloCard(trelloCardId, finishedBlockListId))
-    .then((trelloCardId) => {
-      slackSunbowl.postJobReport({
-        text: `Hey <@nic> & <@jody>,
+    freshbooksSunbowl
+      .addTimeEntry(userName, channelName, parseFloat(timeTaken), videoUrl)
+      .then(() => formstackSunbowl.getTrelloCardId(req.query.channel_name))
+      .then(trelloCardId =>
+        trelloSunbowl.moveTrelloCard(trelloCardId, finishedBlockListId)
+      )
+      .then(trelloCardId => {
+        slackSunbowl.postJobReport({
+          text: `Hey <@nic> & <@jody>,
 <@${userName}> just finished a sprint for <#${req.query.channel_id}>
 Time it took: \`${jobreportArguments[0]} hrs\`
 Video review: ${jobreportArguments[1]}
 Trello card: https://trello.com/c/${trelloCardId}`
+        });
+        slackSunbowl.postToSlack(
+          {
+            text: 'A job report has been submitted and your project is now in the review stage. We will update you as soon as possible when we are ready for your feedback.',
+            response_type: 'in_channel'
+          },
+          req.query.response_url
+        );
+      })
+      .catch(err => {
+        slackSunbowl.postToSlack(
+          utils.constructErrorForSlack(err),
+          req.query.response_url
+        );
       });
-    })
-    .catch((err) => {
-      slackSunbowl.postToSlack(utils.constructErrorForSlack(err), req.query.response_url);
-    });
   });
 };
