83a6624fa25f0bb2b4ef3cb60e53d203dde30bb1
Andrew Golightly <support@andrewgolightly.com>
2017-06-01 16:17:01 -0500

    updated go command to receive post requests

--- commands/deets.js
+++ commands/deets.js
@@ -38,10 +38,10 @@ module.exports = app => {
       })
       .then(descriptionData => {
         const deetsResponse = {
-          text: `*Trello Card Description for ${channel_name}*\n\`\`\`${descriptionData}\`\`\``,
+          text: `*Project Card Details*\n\`\`\`${descriptionData}\`\`\``,
           attachments: [
             {
-              title: 'Click here to go direct to this card in Trello',
+              title: 'Project Details',
               title_link: `https://trello.com/c/${projectsTrelloCardId}`
             }
           ]
