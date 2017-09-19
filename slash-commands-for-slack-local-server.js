1419c6d0258c03e183623199a90621878b624075
Andrew Golightly <support@andrewgolightly.com>
2017-09-19 23:20:22 +0200

    added local server script

--- slash-commands-for-slack-local-server.js
+++ slash-commands-for-slack-local-server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -19,27 +17,12 @@ require('./commands/que')(app);
 require('./actions')(app);
 require('./actions/cron')();
 
-const PORT = 9999;
+const PORT = 3333;
 
-// // Connections are encrypted
-const sslOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/nodesrvr.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/nodesrvr.com/fullchain.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/nodesrvr.com/chain.pem')
-};
-
-// startup the https server
-https.createServer(sslOptions, app).listen(PORT, () => {
+// startup the http server
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(
     `Slash commands for Sunbowl AI started listening on port ${PORT} at ${new Date().toString()}.`
   );
 });
-
-// startup the http server
-// app.listen(PORT, () => {
-//   // eslint-disable-next-line no-console
-//   console.log(
-//     `Slash commands for Sunbowl AI started listening on port ${PORT} at ${new Date().toString()}.`
-//   );
-// });
