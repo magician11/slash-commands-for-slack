33879c6c740b0e4aff76352178238678f80edf90
Andrew Golightly <support@andrewgolightly.com>
2017-10-04 14:17:16 +0200

    updated formstack auth

--- modules/formstack.js
+++ modules/formstack.js
@@ -1,10 +1,12 @@
-/* FORMSTACK */
-const request = require('request');
+/* Formstack */
+
+const request = require("request");
+const config = require("../security/auth.js").get(process.env.NODE_ENV);
 
 function constructEndpointUrl(formId, searchFieldId, searchFieldValue) {
-  const FORMSTACK_TOKEN = process.env.SUNBOWL_FORMSTACK_TOKEN;
-  const FORMSTACK_BASE_URL = 'https://www.formstack.com/api/v2/form';
-  return `${FORMSTACK_BASE_URL}/${formId}/submission.json?data=true&oauth_token=${FORMSTACK_TOKEN}&search_field_0=${searchFieldId}&search_value_0=${searchFieldValue}`;
+  return `${config.formstack
+    .baseURL}/${formId}/submission.json?data=true&oauth_token=${config.formstack
+    .token}&search_field_0=${searchFieldId}&search_value_0=${searchFieldValue}`;
 }
 
 // generic function to fetch data from a Formstack form
@@ -16,15 +18,18 @@ function getDataFromSlackForm(channelName, fieldId) {
         json: true
       },
       (error, response, data) => {
-        data.submissions.forEach((submission) => {
+        data.submissions.forEach(submission => {
           if (submission.data[38710905].value === channelName) {
             if (submission.data[fieldId]) {
               resolve(submission.data[fieldId].value);
             }
           }
         });
-        reject(`Could not find the option with ID ${fieldId} for channel ${channelName}.`);
-      });
+        reject(
+          `Could not find the option with ID ${fieldId} for channel ${channelName}.`
+        );
+      }
+    );
   });
 }
 
@@ -43,14 +48,16 @@ class SunbowlFormstack {
 
   getUsersFreshbooksDetails(userName) {
     return new Promise((resolve, reject) => {
-      request.get({
-        url: constructEndpointUrl(2350444, 41935293, userName),
-        json: true },
+      request.get(
+        {
+          url: constructEndpointUrl(2350444, 41935293, userName),
+          json: true
+        },
         (error, response, data) => {
           if (error) {
             reject(error);
           } else {
-            data.submissions.forEach((submission) => {
+            data.submissions.forEach(submission => {
               if (submission.data[41935293].value === userName) {
                 resolve({
                   usersAPIurl: submission.data[42242492].value,
@@ -61,7 +68,8 @@ class SunbowlFormstack {
             });
             reject(`Could not find an entry for the user ${userName}.`);
           }
-        });
+        }
+      );
     });
   }
 }
